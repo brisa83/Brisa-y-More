15514cb07be7834883b7b8fa66b1fe552a529bbb
brisa83 <brisadagostino4@gmail.com>
2025-04-11 08:33:48 -0300

    Initial commit

---
+++ tp1.py
@@ -0,0 +1,90 @@
+# ejercicio 1
+print("hola mundo")
+
+#ejercicio 2 
+print ("hoy es el dia 25")
+
+#ejercicio 3 
+
+numero = 56
+numero2 = 89
+print (56 + 89 )
+
+#ejercicio 4
+numero= 56
+numero2 = 89
+
+print (f"la suma es {56+89}")
+
+#ejercicio 5 
+
+numero = 15
+print ("su edad es:",numero)
+
+#ejercicio 6
+
+numero = 3.14
+print ("el numero pi es:", numero)
+
+#ejercicio 7 
+
+nombre = "carlitos"
+print ("mi nombre es:", nombre) 
+
+#ejercicio 8 
+
+llueve = True
+
+print ("afuera llueve:", llueve )
+
+#ejercicio 9 
+edad = 23
+print("la edad es", edad)
+
+#ejercicio 10 
+
+edad = 17
+print (f"mi edad es {edad}")
+
+#ejercicio 11 
+
+pi= 3.14
+print ("la variable es", pi )
+
+#ejercicio 12
+
+pi = 3.14
+
+
+print(f"el valor de pi es{pi}")
+
+#ejercicio 13
+nombre= "Brisa"
+
+print ("mi nombre es", nombre)
+
+#ejercicio 14
+nombre = "Morena"
+print (f"mi nombre es {nombre}")
+
+#ejercicio 15
+variable= "llueve"
+print ("hoy", variable)
+
+#ejercicio 16
+llueve= True
+
+print(f"hoy llueve?{llueve}")
+
+#ejercicio 17 
+nombre = input ("ingrese su nombre:")
+print (f"su nombre es{nombre}")
+
+#ejercicio 18 
+nombre = input ("ingrese su nombre: ")
+edad= input ("su edad es: ")
+pi = input ("el valor de pi es: ")
+
+print (f"mi nombre es {nombre} su edad es {edad} el valor de pi es {pi}")
+
+
