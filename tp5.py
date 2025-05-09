fe6c566af1a0167d9cf779503fc565b73f11cbaa
brisa83 <brisadagostino4@gmail.com>
2025-05-09 09:29:37 -0300

    tp n 5

---
+++ tp5.py
@@ -0,0 +1,4 @@
+contraseña = input("ingrese la contraseña")
+while contraseña != "123456":
+    contraseña = input ("la contraseña es incorrecta")
+print ("sesion iniciada")
---
+++ tp5p2.py
@@ -0,0 +1,7 @@
+for i in range (1,5):
+  contraseña =input("ingresa la contraseña")
+  if contraseña != "123456":
+    print ("acceso denegado")
+    break 
+
+
